7669c0321b1406c5c83c4b3cca8234061764dd20
Ney Lucas Vasconcelos Oliviera Santos <neylucas96@gmail.com>
2025-10-06 14:30:23 -0300

    refactor(backend): Extract business logic validation into helper functions

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,6 +1,33 @@
 import userRepository from '../repositories/userRepository.js';
 
 export default {
+  // Função auxiliar para verificar se o usuário a ser atualizado/deletado realmente existe.
+  async _checkUserExistsById(id, operationName) {
+    const idExists = await userRepository.findById(id);
+    if (!idExists) {
+      const err = new Error(`Não existe cadastro com ID: ${id}`);
+      err.statusCode = 404; // Not Found
+      throw err;
+    }
+  },
+
+  // Função auxilixar para verificar se email já foi cadastrado.
+  async _checkEmailIsAvaliable(email, currentUserId = null) {
+    const emailExists = await userRepository.findByEmail(email);
+    currentUserId = parseInt(currentUserId, 10);
+    // Se id for nulo, o objetivo é a criação de um novo usuário com email repetido.
+    // Se id do usuario cujo o email foi retornado for diferente do id enviado nos params,
+    // o usuario não poderá atualizar, pois é de outro usuário.
+    if (
+      emailExists &&
+      (currentUserId === null || emailExists.id !== currentUserId)
+    ) {
+      const err = new Error('Email já cadastrado!');
+      err.statusCode = 409; // Conflict
+      throw err;
+    }
+  },
+
   // Lista todos os usuários.
   async listUsers() {
     const users = await userRepository.findAll();
