571b636c70fbe7c6d3b116882539f24833b9af92
Ney Lucas Vasconcelos Oliviera Santos <neylucas96@gmail.com>
2025-10-21 02:54:21 -0300

    feat: cria uma condicional que usa o SSL dependendo da paridade, se for local ou via nuvem

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,12 +4,26 @@ import dotenv from 'dotenv';
 // Carrega as variáveis de ambiente do arquivo .env
 dotenv.config();
 
+// Configuração SSL Obrigatória (Aiven exige para deploy), se existir nas variáveis de ambiente
+const sslConfig =
+  process.env.USE_SSL === 'true'
+    ? {
+        ssl: {
+          // 'rejectUnauthorized: true' garante que a conexão só ocorra se o certificado for válido.
+          rejectUnauthorized: true,
+        },
+      }
+    : {};
+
 // Cria e exporta um pool de conexões com o banco de dados MySQL.
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
+  port: process.env.MYSQL_PORT || 3306,
+  // Espalha a configuração SSL APENAS se o USE_SSL for 'true'
+  ...sslConfig,
 });
 
 // Função que testa a conexão com o banco antes de inicializar o servidor.
